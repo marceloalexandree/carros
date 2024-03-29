aa4574c47677efffdd746fdd87ba46e37ee02b37
Marcelo Alexandre <marcelo.afilho@eaportal.org>
2024-03-29 13:24:05 -0300

    segundo commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'orange': '#E28625',
+      'green': '#006971',
+      'dark-green': '#004140',
+      'off-white': 'rgba(255, 255, 255, .75)',
+      'white': '#fff',
+      'off-white-bg': '#f2f2f2'
+    },
+  },
+  plugins: [],
+}
