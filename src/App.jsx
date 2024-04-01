4093e3355c232804074d4e070a253649ad421638
Marcelo Alexandre <marcelo.afilho@eaportal.org>
2024-04-01 12:06:04 -0300

    nono commit - resposividade 2: o inimigo agora é outro

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,26 @@ import iconLuxury from "./assets/icon-luxury.svg"
 export default function App() {
   return (
     <main className="min-h-svh bg-off-white-bg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <section className="lg:flex">
-        <div className="bg-orange p-12 rounded-t-lg">
+      <section className="lg:flex mas-w-[920px]">
+        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           <img src={iconSedans} alt="ícone dos sedans" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white">Sedans</h2>
-          <p className="font-lexend mb-7 text-off-white text">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-orange" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+          <p className="font-lexend mb-7 text-off-white text lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-off-white hover:bg-orange hover:text-off-white" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-green p-12">
           <img src={iconSuvs} alt="ícone dos SUVs" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white">SUVs</h2>
-          <p className="font-lexend mb-7 text-off-white text">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras em Santos.</p>
-          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-green" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+          <p className="font-lexend mb-7 text-off-white text lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-off-white hover:bg-green hover:text-off-white" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
-        <div className="bg-dark-green p-12 rounded-b-lg">
+        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
           <img src={iconLuxury} alt="ícone dos Luxuosos" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white">Luxuosos</h2>
-          <p className="font-lexend mb-7 text-off-white text">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-dark-green" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+          <p className="font-lexend mb-7 text-off-white text lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-dark-green border-2  border-off-white hover:bg-dark-green hover:text-off-white" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
         </div>
       </section>
     </main>
