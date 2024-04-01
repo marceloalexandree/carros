92d41d91025fe906f3b9451aca0b98071065a208
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2024-04-01 13:58:57 -0300

    Errei

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Luxury from "./components/Luxury"
 export default function App() {
   return (
     <main className="min-h-svh bg-off-white-bg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <div className="lg:flex mas-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans/>
         <Suvs/>
         <Luxury/>
