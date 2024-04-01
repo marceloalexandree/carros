67335b9016fd01e25cde31f3e7e4cb8e7294f025
Marcelo Alexandre <marcelo.afilho@eaportal.org>
2024-04-01 11:33:50 -0300

    sétimo commit - versão nokia tijolão

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <div className="bg-green p-12">
           <img src={iconSuvs} alt="ícone dos SUVs" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white">SUVs</h2>
-          <p className="font-lexend mb-7 text-off-white text">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <p className="font-lexend mb-7 text-off-white text">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras em Santos.</p>
           <a className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-green" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
